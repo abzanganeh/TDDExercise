c69cb75deb6eb39b43c4c776cb069da1f051b7fb
Alireza BarzinZanganeh <abarzinzanganeh@gmail.com>
2018-04-28 18:57:46 -0700

    UI first set of changes

--- app/src/main/java/com/barzinzanganeh/tddexercise/MainActivity.java
+++ app/src/main/java/com/barzinzanganeh/tddexercise/MainActivity.java
@@ -1,13 +1,38 @@
 package com.barzinzanganeh.tddexercise;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.first_name_value)
+    TextView firstNameValue;
+    @BindView(R.id.last_name_value)
+    TextView lastNameValue;
+    @BindView(R.id.age_value)
+    TextView ageValue;
+    @BindView(R.id.save_button)
+    Button saveButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        saveButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "The message is saved", Toast.LENGTH_SHORT).show();
+
     }
 }
